287446ed85172bf7ad4a7afc6e0af10abd7f8dea
BigBlueTree <124953905+BigBlueTree@users.noreply.github.com>
2023-05-02 13:58:07 +0900

    Add files via upload

---
+++ 0502/demo/src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class UserDto {
+    private String name;
+    private Integer age;
+}
